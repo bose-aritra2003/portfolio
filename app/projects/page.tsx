745fafa34db94460fae3a027fd5f06e00d88721f
Aritra Bose <aritrabose2003@gmail.com>
2023-07-07 00:32:13 +0530

    finalise v0.1.0

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,6 +2,38 @@ import Hero from "@/components/projects/Hero";
 import Showcase from "@/components/projects/Showcase";
 import Intro from "@/components/projects/Intro";
 
+export const metadata = {
+  title: 'Aritra Bose - Projects',
+  description: 'A list of projects made by Aritra Bose',
+  openGraph: {
+    type: 'website',
+    locale: 'en_IN',
+    url: 'https://aritrabose.vercel.app',
+    title: "Aritra Bose - Projects",
+    siteName: 'Aritra Bose',
+    description: "A list of projects made by Aritra Bose",
+    images: [
+      {
+        url: 'icon.svg',
+        width: 256,
+        height: 256,
+      }
+    ],
+  },
+  twitter: {
+    title: "Aritra Bose - Projects",
+    description: "A list of projects made by Aritra Bose",
+    card: "summary_large_image",
+    images: [
+      {
+        url: 'apple-icon.png',
+        width: 256,
+        height: 256,
+      }
+    ],
+  }
+}
+
 const Projects = () => {
   return (
     <>
