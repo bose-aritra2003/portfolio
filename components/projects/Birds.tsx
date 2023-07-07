b0e63898854ac3d38c6224a2c5a1d99e0c00cb3c
Aritra Bose <aritrabose2003@gmail.com>
2023-07-07 18:16:09 +0530

    fix responsiveness of projects page

--- components/projects/Birds.tsx
+++ components/projects/Birds.tsx
@@ -4,14 +4,17 @@ const Birds = () => {
   return (
     <>
       <div className="bird-container bird-container--one">
-        <div className="bird bird--one drop"></div>
+        <div className="bird bird--one"></div>
       </div>
+
       <div className="bird-container bird-container--two">
         <div className="bird bird--two"></div>
       </div>
+
       <div className="bird-container bird-container--three">
         <div className="bird bird--three"></div>
       </div>
+
       <div className="bird-container bird-container--four">
         <div className="bird bird--four"></div>
       </div>
